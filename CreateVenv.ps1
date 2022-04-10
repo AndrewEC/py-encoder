db9ccf512896348223428cc2c3c6b5a7e33004f7
unknown <andrew.e.cumming@gmail.com>
2022-04-10 18:07:53 -0400

    Updated create venv script to make it more generic.

--- CreateVenv.ps1
+++ CreateVenv.ps1
@@ -1,12 +1,10 @@
-$EnvFolder = "encoder-venv"
+$CurrentFolder = Get-Location | Split-Path -Leaf
+$EnvFolder = "$CurrentFolder-venv"
 
 if (Test-Path $EnvFolder) {
     Invoke-Expression "./$EnvFolder/Scripts/Activate.ps1"
 } else {
-    Write-Host "Creating virtual environment: $EnvFolder"
     python -m venv $EnvFolder `
         && Invoke-Expression "./$EnvFolder/Scripts/Activate.ps1" `
         && pip install -r requirements.txt `
-
-    cd package && python setup.py install && cd ..
 }
